772dffec0b3697b9d8c9e0a38fb42431005e96d6
Stefan Schinkel <mail@dreeg.org>
2019-09-06 21:03:40 +0200

    [setup] refine setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#from distutils.core import setup, Extension
-
 from os import path
-from setuptools import setup, find_packages
+from setuptools import setup
 
-here=path.abspath(path.dirname(__file__))
+cwd = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'Readme.md'), encoding='utf-8') as f:
-    long_description=f.read()
+with open(path.join(cwd, "Readme.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
-    name='gantt',
-    version='0.1',
-    author='Stefan Schinkel',
-    author_email='mail@dreeg.org',
-    license ='license.txt',
+    name="gantt",
+    version="1.0",
+    author="Stefan Schinkel",
+    author_email="mail@dreeg.org",
+    license="License :: OSI Approved :: MIT License",
     keywords="gantt, project management",
     platforms=[
         "Operating System :: OS Independent",
     ],
     classifiers=[
-        "Development Status :: Development Status :: 4 - Beta",
-        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Environment :: Console",
         "Intended Audience :: Developers",
@@ -32,17 +32,17 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Office/Business :: Scheduling",
     ],
-    # packages=['gantt'],
-    url='https://github.com/stefanSchinkel/gantt',
-    description='Simple gantt charts in python.',
+    # packages=["gantt"],
+    url="https://github.com/stefanSchinkel/gantt",
+    description="Simple gantt charts in python.",
     long_description=long_description,
     install_requires=[
-        "matplotlib==3.0.3",
-        "numpy==1.16.3"
+        "matplotlib>=3.0.3",
+        "numpy>=1.16.3"
     ],
     setup_requires=[
-        "matplotlib==3.0.3",
-        "numpy==1.16.3"
+        "matplotlib>=3.0.3",
+        "numpy>=1.16.3"
     ],
     include_package_data=True,
 )
